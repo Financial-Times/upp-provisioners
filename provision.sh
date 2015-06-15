e502e392a57b348703da2f7b69ba63b3e814a257
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-06-15 16:53:01 +0300

    added provision script

---
+++ provision.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -z "$AWS_ACCESS_KEY_ID" ] && [ ! -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "first"
+    . .venv/bin/activate && echo $VAULT_PASS > /vault.pass && ansible-playbook -i ~/.ansible_hosts aws_coreos_site.yml --extra-vars "token=$TOKEN_URL aws_access_key_id=$AWS_ACCESS_KEY_ID aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" --vault-password-file=/vault.pass 
+else
+    echo "second"
+    . .venv/bin/activate && echo $VAULT_PASS > /vault.pass && ansible-playbook -i ~/.ansible_hosts aws_coreos_site.yml --extra-vars "token=$TOKEN_URL" --vault-password-file=/vault.pass 
+fi
