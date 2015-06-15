217f7f834b2486e16f1e87a0b294ecf6d39dbc83
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-06-15 17:17:32 +0300

    removed noise

--- provision.sh
+++ provision.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [ ! -z "$AWS_ACCESS_KEY_ID" ] && [ ! -z "$AWS_SECRET_ACCESS_KEY" ]; then
-    echo "first"
     . .venv/bin/activate && echo $VAULT_PASS > /vault.pass && ansible-playbook -i ~/.ansible_hosts aws_coreos_site.yml --extra-vars "token=$TOKEN_URL deployer_service_file_location=$DEPLOYER_SERVICE_FILE_LOCATION aws_access_key_id=$AWS_ACCESS_KEY_ID aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" --vault-password-file=/vault.pass 
 else
-    echo "second"
     . .venv/bin/activate && echo $VAULT_PASS > /vault.pass && ansible-playbook -i ~/.ansible_hosts aws_coreos_site.yml --extra-vars "token=$TOKEN_URL deployer_service_file_location=$DEPLOYER_SERVICE_FILE_LOCATION" --vault-password-file=/vault.pass 
 fi
