d0ec3c01463c1da2b840858754acd27ee9e17f49
Jussi.Heinonen <jussi.heinonen@ft.com>
2016-12-12 16:38:41 +0000

    Set default java to 1.8

--- puppet/neo4jha/manifests/install.pp
+++ puppet/neo4jha/manifests/install.pp
@@ -7,8 +7,13 @@ class neo4jha::install {
     command => "tar -zxvf ${::neo4jha::parentdir}/${::neo4jha::package}"
   }
 
-  package {'java-1.7.0-openjdk': ensure => absent, allow_virtual => true }
-  ->
   package { 'java-1.8.0-openjdk': ensure => present, allow_virtual => true }
+  ->
+  # Switch default java to 1.8 if not already done so
+  file { '/etc/alternatives/java':
+    ensure  => link,
+    target  => '/usr/lib/jvm/jre-1.8.0-openjdk/bin/java'
+  }
+
 
 }
