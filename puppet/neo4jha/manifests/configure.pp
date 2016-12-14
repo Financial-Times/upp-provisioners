f0cc8a699c09fef06eb7bd7f852eb3000ff6a281
Jussi.Heinonen <jussi.heinonen@ft.com>
2016-12-14 09:30:29 +0000

    Create link for neo4j logs directory pointing to /var/log/apps/neo4j for storing logs on /var partition

--- puppet/neo4jha/manifests/configure.pp
+++ puppet/neo4jha/manifests/configure.pp
@@ -10,4 +10,17 @@ class neo4jha::configure {
     content => template("${module_name}/neo4j.sh");
   }
 
+  file { ['/var/log/apps', '/var/log/apps/neo4j']:
+    owner    => "${::neo4jha::username}",
+    group    => "${::neo4jha::username}",
+    ensure  => directory,
+  }
+  ->
+  file { "${::neo4jha::neo4j_home}/logs":
+    owner    => "${::neo4jha::username}",
+    group    => "${::neo4jha::username}",
+    force   => true,
+    ensure  => link,
+    target  => "/var/log/apps/neo4j",
+  }
 }
