1521750b13a3eace5dc62ef20c9da833eede0bbc
Jussi.Heinonen <jussi.heinonen@ft.com>
2016-12-19 10:47:45 +0000

    Update initial_hosts.sh

--- sh/initial_hosts.sh
+++ sh/initial_hosts.sh
@@ -6,7 +6,7 @@ declare -a ELBS
 STACKNAME=$(getStackName)
 
 time for each in $(aws elb --region $(getRegion) describe-load-balancers --output text --query LoadBalancerDescriptions[].DNSName); do
-  if [[ "$(echo $each | grep -i "internal-${STACKNAME}" > /dev/null; echo $?)" == "0" ]]; then
+  if [[ "$(echo $each | grep -i "internal-${STACKNAME}-" > /dev/null; echo $?)" == "0" ]]; then
     ELBS+=( "$each" )
   fi
 done
@@ -14,5 +14,7 @@ done
 if [[ "${#ELBS[*]}" -eq "0" ]]; then #If number of records is 0 fail visibly
   errorAndExit "Failed to find any ELB records for stack named ${STACKNAME}" 1
 else
-  info "Number of records ${#ELBS[*]}: ${ELBS[*]}"
+  for each in ${ELBS[*]}; do
+    INITIAL_HOSTS+="${each}:5000,"
+    echo "${INITIAL_HOSTS::-1}" #echo without trailing comma
 fi
