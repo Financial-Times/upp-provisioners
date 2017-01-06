1b1442d54b077368d2e0ef054625da14db1f0082
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-01-06 10:50:05 +0000

    Remove mkfs command as EBS volume is built from snapshot which already contains filesystem

--- sh/bootstrap.sh
+++ sh/bootstrap.sh
@@ -8,8 +8,6 @@ echo "Setting hostname in AWS console" >> ${OUTPUT}
 /usr/bin/aws ec2 --region $(/usr/bin/wget -q -O - http://169.254.169.254/latest/meta-data/hostname | cut -d . -f 2) delete-tags --resources $(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id) --tags Key=Name
 /usr/bin/aws ec2 --region $(/usr/bin/wget -q -O - http://169.254.169.254/latest/meta-data/hostname | cut -d . -f 2) create-tags --resources $(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id) --tags Key=Name,Value=$(wget -q -O - http://169.254.169.254/latest/meta-data/hostname)
 echo "Hostname set" >> ${OUTPUT}
-# Create ext4 filesystem on EBS volume
-/sbin/mkfs.ext4 /dev/sdc >> ${OUTPUT}
 
 # Install packages for deployment
 /usr/bin/yum install -y puppet3 git tcpdump >> ${OUTPUT}
