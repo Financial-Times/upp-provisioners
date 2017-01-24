e9476d500e6eae8f36946e00793536a3948b7ea5
David Smith <david.smith@ft.com>
2017-01-24 14:12:40 +0000

    Provision script + added environment variables to be passed in user data.

---
+++ provision.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+echo "Creating stack for environment ${ENVIRONMENT_TAG:?No environment tag provided.}"
+set AWS_ACCESS_KEY_ID = "${AWS_ACCESS_KEY_ID:?AWS Access Key not set.}"
+set AWS_SECRET_ACCESS_KEY = "${AWS_SECRET_ACCESS_KEY:?AWS Secret Access Key not set.}"
+set SERVICES_DEFINITION_ROOT_URI = "${SERVICES_DEFINITION_ROOT_URI:?Service file definition not set.}"
+set SPLUNK_HEC_TOKEN = "${SPLUNK_HEC_TOKEN:?Splunk HEC Token not set.}"
+set SPLUNK_HEC_URL = "${SPLUNK_HEC_URL:?Splunk HEC URL not set.}"
+set KONSTRUCTOR_API_KEY = "${KONSTRUCTOR_API_KEY:?Konstructor API Key not set.}"
+set TOKEN_URL = `curl -s https://discovery.etcd.io/new?size=3`
+
+read -r -d '' CF_PARAMS <<EOM
+[
+    { 
+        "ParameterKey": "KonstructorAPIKey",
+        "ParameterValue": "${KONSTRUCTOR_API_KEY}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "ServicesDefinitionRootURI",
+        "ParameterValue": "${SERVICES_DEFINITION_ROOT_URI}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "CocoEnvironmentTag",
+        "ParameterValue": "${ENVIRONMENT_TAG}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "SplunkHecURL",
+        "ParameterValue": "${SPLUNK_HEC_URL}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "SplunkHecToken",
+        "ParameterValue": "${SPLUNK_HEC_TOKEN}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "EtcdToken",
+        "ParameterValue": "${TOKEN_URL}",
+        "UsePreviousValue": false
+    }
+]
+EOM
+
+aws cloudformation create-stack --stack-name=up-neo4j-${ENVIRONMENT_TAG} --template-body=file://./cloudformation/neo4jhacluster.yaml --parameters="${CF_PARAMS}"
