9d6cca531d8317e90ca049ae176716003cdbe504
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-01-26 11:26:35 +0000

    Add logic to jumpbox.sh detect whether it's running on docker or not

--- sh/jumpbox.sh
+++ sh/jumpbox.sh
@@ -1,20 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+source $(dirname $0)/functions.sh || echo "Failed to source functions.sh"
 
 OUTPUT="/var/log/jumpbox.log"
 ROOTDIR='/tmp'
-echo  "BEGIN - $(date)" >> ${OUTPUT}
-# Update Name tag
-if [[ "$(ping -c 1 -w 1 169.254.169.254 ; echo $?)" -ne "1" ]]; then
-  echo "Setting hostname in AWS console" >> ${OUTPUT}
+
+info  "BEGIN - $(date)" | tee ${OUTPUT}
+
+if [[ "${PPID}" -eq "1" ]]; then
+  info "Parent PID 1 indicates we are running inside Docker container, skip tagging and package installation" | tee ${OUTPUT}
+else
+  # Update Name tag
+  info "Setting hostname in AWS console" | tee ${OUTPUT}
   /usr/bin/aws ec2 --region $(/usr/bin/wget -q -O - http://169.254.169.254/latest/meta-data/hostname | cut -d . -f 2) delete-tags --resources $(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id) --tags Key=Name
   /usr/bin/aws ec2 --region $(/usr/bin/wget -q -O - http://169.254.169.254/latest/meta-data/hostname | cut -d . -f 2) create-tags --resources $(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id) --tags Key=Name,Value=$(wget -q -O - http://169.254.169.254/latest/meta-data/hostname)
-  echo "Hostname set" >> ${OUTPUT}
+  info "Hostname set" | tee ${OUTPUT}
   # Install packages for deployment
-  /usr/bin/yum install -y puppet3 git >> ${OUTPUT}
-  puppet --version >> ${OUTPUT}
-  git --version >> ${OUTPUT}
-else
-  echo "Host 169.254.169.254 not responding. Skipping tagging." >> ${OUTPUT}
+  /usr/bin/yum install -y puppet3 git | tee ${OUTPUT}
+  puppet --version | tee ${OUTPUT}
+  git --version | tee ${OUTPUT}
 fi
 
 cd ${ROOTDIR}
@@ -22,10 +26,10 @@ if [[ -d "./up-neo4j-ha-cluster" ]]; then
     cd up-neo4j-ha-cluster/
     git pull
 else
-  git clone https://github.com/Financial-Times/up-neo4j-ha-cluster.git >> ${OUTPUT}
+  git clone https://github.com/Financial-Times/up-neo4j-ha-cluster.git | tee ${OUTPUT}
   cd up-neo4j-ha-cluster/
 fi
 
-puppet apply --modulepath ./puppet -e "class { 'neo4jha::jumpbox': }" >> ${OUTPUT}
+puppet apply --modulepath ./puppet -e "class { 'neo4jha::jumpbox': }" | tee ${OUTPUT}
 sh/authorized_keys.sh
-echo  "END - $(date)" >> ${OUTPUT}
+info  "END - $(date)" | tee ${OUTPUT}
