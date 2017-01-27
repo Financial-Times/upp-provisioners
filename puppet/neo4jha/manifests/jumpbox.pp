b267ba8d59669cfe97e0bc16e1ad6e1de6ac5ac0
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-01-27 11:44:14 +0000

    remove tee command from crontab as tee is referenced by authorized_keys.service.sh

--- puppet/neo4jha/manifests/jumpbox.pp
+++ puppet/neo4jha/manifests/jumpbox.pp
@@ -1,7 +1,7 @@
 class neo4jha::jumpbox {
 
   cron { 'authorized_keys-service':
-    command => "/usr/bin/curl -s https://raw.githubusercontent.com/Financial-Times/up-neo4j-ha-cluster/master/sh/authorized_keys.service.sh | /bin/bash | tee -a /var/log/jumpbox.log",
+    command => "/usr/bin/curl -s https://raw.githubusercontent.com/Financial-Times/up-neo4j-ha-cluster/master/sh/authorized_keys.service.sh | /bin/bash",
     user    => 'root',
     minute  => '*/5',
   }
