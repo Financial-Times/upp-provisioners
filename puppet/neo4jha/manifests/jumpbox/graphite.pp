7ed6a8f0616543e52149b2eca375b416c517bd1d
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-01-30 14:09:38 +0000

    Fix syntax in graphite.pp

--- puppet/neo4jha/manifests/jumpbox/graphite.pp
+++ puppet/neo4jha/manifests/jumpbox/graphite.pp
@@ -1,14 +1,17 @@
 class neo4jha::jumpbox::graphite {
-  package { 'collectd': ensure => 'present', }
+  package { 'collectd':
+   ensure         => 'present',
+   allow_virtual  => false,
+   }
   ->
   file { '/etc/collectd.d':
     ensure  => 'directory',
     recurse => true,
-    source => "puppet:///${module_name}/graphite",
+    source  => "puppet:///${module_name}/graphite",
   }
   ~>
   service { 'collectd':
-    ensure => 'running',
-    enable => true, }
+    ensure  => 'running',
+    enable  => true, }
 
 }
