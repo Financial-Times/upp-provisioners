7266c50835d697f5755c06723cc0777a93428a5e
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-02-08 13:57:26 +0000

    Workaround issue with TOKEN_URL

--- provision.sh
+++ provision.sh
@@ -7,47 +7,47 @@ set SERVICES_DEFINITION_ROOT_URI = "${SERVICES_DEFINITION_ROOT_URI:?Service file
 set SPLUNK_HEC_TOKEN = "${SPLUNK_HEC_TOKEN:?Splunk HEC Token not set.}"
 set SPLUNK_HEC_URL = "${SPLUNK_HEC_URL:?Splunk HEC URL not set.}"
 set KONSTRUCTOR_API_KEY = "${KONSTRUCTOR_API_KEY:?Konstructor API Key not set.}"
-set TOKEN_URL = `curl -s https://discovery.etcd.io/new?size=3`
+TOKEN_URL=$(curl -s https://discovery.etcd.io/new?size=3)
+set TOKEN_URL = "${TOKEN_URL:?Etcd2 token url not set.}"
 set NEO_EXTRA_CONF_URL = "${NEO_EXTRA_CONF_URL:?Neo4J Extra Conf URL not provided.}"
 
 read -r -d '' CF_PARAMS <<EOM
 [
-    { 
+    {
         "ParameterKey": "KonstructorAPIKey",
         "ParameterValue": "${KONSTRUCTOR_API_KEY}",
         "UsePreviousValue": false
     },
-    { 
+    {
         "ParameterKey": "ServicesDefinitionRootURI",
         "ParameterValue": "${SERVICES_DEFINITION_ROOT_URI}",
         "UsePreviousValue": false
     },
-    { 
+    {
         "ParameterKey": "CocoEnvironmentTag",
         "ParameterValue": "${ENVIRONMENT_TAG}",
         "UsePreviousValue": false
     },
-    { 
+    {
         "ParameterKey": "SplunkHecURL",
         "ParameterValue": "${SPLUNK_HEC_URL}",
         "UsePreviousValue": false
     },
-    { 
+    {
         "ParameterKey": "SplunkHecToken",
         "ParameterValue": "${SPLUNK_HEC_TOKEN}",
         "UsePreviousValue": false
     },
-    { 
+    {
         "ParameterKey": "EtcdToken",
         "ParameterValue": "${TOKEN_URL}",
         "UsePreviousValue": false
     },
-    { 
+    {
         "ParameterKey": "ExtraNeoConf",
         "ParameterValue": "${NEO_EXTRA_CONF_URL}",
         "UsePreviousValue": false
     }
 ]
 EOM
-
 aws cloudformation create-stack --stack-name=up-neo4j-${ENVIRONMENT_TAG} --template-body=file://./cloudformation/neo4jhacluster.yaml --parameters="${CF_PARAMS}"
