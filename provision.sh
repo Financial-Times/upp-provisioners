9ef5f2dd9d3a88120be2144f3ad72bd627ddff2c
David Smith <david.smith@ft.com>
2017-02-16 11:19:21 +0000

    Tidy Cloudformation script (#2)
    * Tidy up
    - Put parameters into a logical order
    - Removed some hang overs from Amz AMI.
    - Ensured tags matched FT standards
    - Added in some comments.
    
    * Provisioner can now deploy to both regions
    
    * Let Environment tag be changed

--- provision.sh
+++ provision.sh
@@ -1,18 +1,77 @@
 #!/usr/bin/env bash
 
 echo "Creating stack for environment ${ENVIRONMENT_TAG:?No environment tag provided.}"
-set AWS_ACCESS_KEY_ID = "${AWS_ACCESS_KEY_ID:?AWS Access Key not set.}"
-set AWS_SECRET_ACCESS_KEY = "${AWS_SECRET_ACCESS_KEY:?AWS Secret Access Key not set.}"
-set SERVICES_DEFINITION_ROOT_URI = "${SERVICES_DEFINITION_ROOT_URI:?Service file definition not set.}"
-set SPLUNK_HEC_TOKEN = "${SPLUNK_HEC_TOKEN:?Splunk HEC Token not set.}"
-set SPLUNK_HEC_URL = "${SPLUNK_HEC_URL:?Splunk HEC URL not set.}"
-set KONSTRUCTOR_API_KEY = "${KONSTRUCTOR_API_KEY:?Konstructor API Key not set.}"
-TOKEN_URL="$(curl --connect-timeout 5 -s https://discovery.etcd.io/new?size=3)"
-set TOKEN_URL = "${TOKEN_URL:?Failed to get etcd2 token URL}"
-set NEO_EXTRA_CONF_URL = "${NEO_EXTRA_CONF_URL:?Neo4J Extra Conf URL not provided.}"
+AWS_ACCESS_KEY_ID = "${AWS_ACCESS_KEY_ID:?AWS Access Key not set.}"
+AWS_SECRET_ACCESS_KEY = "${AWS_SECRET_ACCESS_KEY:?AWS Secret Access Key not set.}"
+SERVICES_DEFINITION_ROOT_URI = "${SERVICES_DEFINITION_ROOT_URI:?Service file definition not set.}"
+SPLUNK_HEC_TOKEN = "${SPLUNK_HEC_TOKEN:?Splunk HEC Token not set.}"
+SPLUNK_HEC_URL = "${SPLUNK_HEC_URL:?Splunk HEC URL not set.}"
+KONSTRUCTOR_API_KEY = "${KONSTRUCTOR_API_KEY:?Konstructor API Key not set.}"
+TOKEN_URL = "$(curl --connect-timeout 5 -s https://discovery.etcd.io/new?size=3)"
+TOKEN_URL = "${TOKEN_URL:?Failed to get etcd2 token URL}"
+NEO_EXTRA_CONF_URL = "${NEO_EXTRA_CONF_URL:?Neo4J Extra Conf URL not provided.}"
+AWS_DEFAULT_REGION = "${AWS_DEFAULT_REGION:?AWS Region not set.}"
+ENVIRONMENT_TYPE = "${ENVIRONMENT_TYPE:?Environment type not set.}"
+
+case "$AWS_DEFAULT_REGION" in
+    eu-west-1) 
+        VPC_ID = "vpc-36639c52"
+        SUBNET1 = "subnet-b59b54c3"
+        SUBNET2 = "subnet-1cba5f44"
+        SUBNET3 = "subnet-fcfa5e98"
+        SNAPSHOT = "snap-08ce671d2f33e9e5d"
+        AMI = "ami-ac8fd4ca"
+        ;;
+    us-east-1)
+        VPC_ID = "vpc-1d25657a"
+        SUBNET1 = "subnet-5a978b02"
+        SUBNET2 = "subnet-b9c608f0"
+        SUBNET3 = "subnet-b5b5aa9f"
+        SNAPSHOT = "snap-0c0978a76e7255bcd"
+        AMI = "ami-fd6c94eb"
+        ;;
+    *)
+        echo "Can only be deployed in eu-west-1 or us-east-1"
+        exit 1
+esac
 
 read -r -d '' CF_PARAMS <<EOM
 [
+    { 
+        "ParameterKey": "TagEnvironment",
+        "ParameterValue": "${ENVIRONMENT_TYPE}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "ImageId",
+        "ParameterValue": "${AMI}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "EbsSnapshot",
+        "ParameterValue": "${SNAPSHOT}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "VPC",
+        "ParameterValue": "${VPC_ID}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "Subnet1",
+        "ParameterValue": "${SUBNET1}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "Subnet2",
+        "ParameterValue": "${SUBNET2}",
+        "UsePreviousValue": false
+    },
+    { 
+        "ParameterKey": "Subnet3",
+        "ParameterValue": "${SUBNET3}",
+        "UsePreviousValue": false
+    },
     { 
         "ParameterKey": "AWSAccessKeyId",
         "ParameterValue": "${AWS_ACCESS_KEY_ID}",
