c936caafe7a20bd47b740df98bbe5cba59d55784
Euan.Finlay <euan.finlay@ft.com>
2017-03-16 09:21:36 +0000

    adding extra args to provision script

--- sh/provision.sh
+++ sh/provision.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Create Ansible vault credentials
-echo $VAULT_PASS > /ansible/vault.pass
+echo ${VAULT_PASS} > /ansible/vault.pass
 
 cd /ansible
-ansible-playbook --vault-password-file=vault.pass provision.yml
+ansible-playbook --vault-password-file=vault.pass provision.yml --extra-vars "\
+cluster_name=${CLUSTER_NAME} \
+aws_default_region=${AWS_DEFAULT_REGION} \
+cf_template=${CF_TEMPLATE} \
+environment_type=${ENVIRONMENT_TYPE}"
