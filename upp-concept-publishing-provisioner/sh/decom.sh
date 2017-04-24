65440d17ecc9b4eebad411370a3266d604606c25
Dhas <jermila.dhas@ft-mw4834.ad.ft.com>
2017-04-24 14:48:56 +0100

    Bash scripts to initiate the ansible playbooks for provisioning and decommissioning

---
+++ upp-concept-publishing-provisioner/sh/decom.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create Ansible vault credentials
+echo $VAULT_PASS > /ansible/vault.pass
+
+cd /ansible
+
+ansible-playbook --vault-password-file=vault.pass decom.yml --extra-vars "\
+cluster_name="upp-${CLUSTER_NAME}" \
+aws_default_region=${AWS_DEFAULT_REGION} \
+environment_tag=${ENVIRONMENT_TAG}"
