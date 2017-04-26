99c3a82f64c53158fff0d37a821e42db9dfd9c1c
Jason Zoidis <jason.zoidis@ft.com>
2017-04-26 12:10:25 +0000

    updating scripts with new git repo

--- upp-neo4j-provisioner/puppet/neo4jha/manifests/jumpbox.pp
+++ upp-neo4j-provisioner/puppet/neo4jha/manifests/jumpbox.pp
@@ -12,7 +12,7 @@ class neo4jha::jumpbox {
   class { 'neo4jha::collectd': }
 
   cron { 'authorized_keys-service':
-    command => "/usr/bin/curl -s https://raw.githubusercontent.com/Financial-Times/up-neo4j-ha-cluster/master/sh/authorized_keys.service.sh | /bin/bash",
+    command => "/usr/bin/curl -s https://raw.githubusercontent.com/Financial-Times/upp-provisioners/master/upp-neo4j-provisioner/sh/authorized_keys.service.sh | /bin/bash",
     user    => 'root',
     minute  => '*/5',
   }
