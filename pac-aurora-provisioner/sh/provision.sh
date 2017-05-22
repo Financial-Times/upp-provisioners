c0dcda40aa1cefda61aa7122b8ee4aa41729f13d
Dhas <jermila.dhas@ft-mw4834.ad.ft.com>
2017-05-22 19:44:07 +0100

    * Update README * Use two vault files for test and production * Use cloudformation stack for creating a cluster in eu * Use aws cli to provision a cross region read replica

--- pac-aurora-provisioner/sh/provision.sh
+++ pac-aurora-provisioner/sh/provision.sh
@@ -9,7 +9,9 @@ ansible-playbook --vault-password-file=vault.pass provision.yml --extra-vars "\
 cluster=${CLUSTER} \
 environment_type=${ENVIRONMENT_TYPE} "
 
-# Creation of cross region read replica to be replaced with cloud formation script
+# TO BE REPLACED BY CLOUDFORMATION
+
+# Creation of cross region read replica
 # Set AWS credentials
 export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
 export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
@@ -29,12 +31,12 @@ done
 # Create resources for the DB cluster in us-east-1
 # DB parameter group, Cluster parameter group
 
-DB_PARAM_GROUP=$(aws rds create-db-parameter-group --db-parameter-group-name=pac-aurora-${CLUSTER}-dbgroup --db-parameter-group-family=aurora5.6 --description='PAC db parameter group')
-CLUSTER_PARAM_GROUP=$(aws rds create-db-cluster-parameter-group --db-cluster-parameter-group-name=pac-aurora-${CLUSTER}-clustergroup --db-parameter-group-family=aurora5.6 --description='PAC cluster parameter group')
+aws rds create-db-parameter-group --db-parameter-group-name=pac-aurora-${CLUSTER}-dbgroup --db-parameter-group-family=aurora5.6 --description='PAC db parameter group'
+aws rds create-db-cluster-parameter-group --db-cluster-parameter-group-name=pac-aurora-${CLUSTER}-clustergroup --db-parameter-group-family=aurora5.6 --description='PAC cluster parameter group'
 
 # Create a DB cluster
-CLUSTER_NAME=$(aws rds create-db-cluster --db-cluster-identifier=pac-aurora-${CLUSTER}-useast --db-cluster-parameter-group-name=${CLUSTER_PARAM_GROUP} --vpc-security-group-ids=sg-6b79a215 --db-subnet-group-name=aurora-db-subnet-group --replication-source-identifier=${clusterARN} --engine=aurora)
+aws rds create-db-cluster --db-cluster-identifier=pac-aurora-${CLUSTER}-useast --db-cluster-parameter-group-name=pac-aurora-${CLUSTER}-clustergroup --vpc-security-group-ids=sg-6b79a215 --db-subnet-group-name=pac-aurora-subnet-group --replication-source-identifier=${clusterARN} --engine=aurora
 
 # Create two instances and register with the DB cluster
-aws rds create-db-instance --db-cluster-identifier=${CLUSTER_NAME} --db-instance-class=db.t2.small --engine=aurora --db-instance-identifier=pac-aurora-${CLUSTER}-1 --db-parameter-group-name=${DB_PARAM_GROUP} --no-publicly-accessible --db-subnet-group-name=aurora-db-subnet-group
-aws rds create-db-instance --db-cluster-identifier=${CLUSTER_NAME} --db-instance-class=db.t2.small --engine=aurora --db-instance-identifier=pac-aurora-${CLUSTER}-2 --db-parameter-group-name=${DB_PARAM_GROUP} --no-publicly-accessible --db-subnet-group-name=aurora-db-subnet-group
+aws rds create-db-instance --db-cluster-identifier=pac-aurora-${CLUSTER}-useast --db-instance-class=db.t2.small --engine=aurora --db-instance-identifier=pac-aurora-${CLUSTER}-1 --db-parameter-group-name=pac-aurora-${CLUSTER}-dbgroup --no-publicly-accessible --db-subnet-group-name=pac-aurora-subnet-group
+aws rds create-db-instance --db-cluster-identifier=pac-aurora-${CLUSTER}-useast --db-instance-class=db.t2.small --engine=aurora --db-instance-identifier=pac-aurora-${CLUSTER}-2 --db-parameter-group-name=pac-aurora-${CLUSTER}-dbgroup --no-publicly-accessible --db-subnet-group-name=pac-aurora-subnet-group
