a673a353fb7227da9e5343639eb8bf56e4e4f975
Euan Finlay <euan.finlay@ft.com>
2017-06-06 15:39:54 +0100

    rewriting susbstitution script to cope with values containing equals symbols, multiple matching key names, and values containing spaces

--- upp-pub-provisioner/sh/substitute-ft-env-variables.sh
+++ upp-pub-provisioner/sh/substitute-ft-env-variables.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-for variable in `cat /tmp/ft-env-variables` ; do
+while read variable ; do
     # this splits the key value pairs into two separate variables
     # for more info on how it works:
+    # https://stackoverflow.com/a/1521498
     # https://unix.stackexchange.com/a/53315
-    IFS='=' read -r key value <<< "$variable"
+    key=$(cut -d '=' -f 1 <<< "$variable")
+    value=$(cut -d '=' -f 2- <<< "$variable")
 
-    sed -i "s|\$${key}|${value}|g" /tmp/persistent_instance_user_data.yaml
-done
+    sed -i "s|'\$${key}'|'${value}'|g" /tmp/persistent_instance_user_data.yaml
+done < /tmp/ft-env-variables
