bdd45c03be278451baeb90cdcec6bdd6060ab51f
Euan Finlay <euan.finlay@ft.com>
2017-06-06 15:03:55 +0100

    fixing bug with substituting key/values that contain equals symbols

--- upp-pub-provisioner/sh/substitute-ft-env-variables.sh
+++ upp-pub-provisioner/sh/substitute-ft-env-variables.sh
@@ -3,9 +3,8 @@
 for variable in `cat /tmp/ft-env-variables` ; do
     # this splits the key value pairs into two separate variables
     # for more info on how it works:
-    # http://stackoverflow.com/a/19482947
-    key=${variable%=*}
-    value=${variable##*=}
+    # https://unix.stackexchange.com/a/53315
+    IFS='=' read -r key value <<< "$variable"
 
     sed -i "s|\$${key}|${value}|g" /tmp/persistent_instance_user_data.yaml
 done
