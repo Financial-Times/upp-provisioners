1d4fd14e7edc6053fb3624a434aefd2ce61d5141
Euan Finlay <euan.finlay@ft.com>
2017-06-09 16:36:19 +0100

    Revert "Fix userdata variable substitution"

--- upp-pub-provisioner/sh/substitute-ft-env-variables.sh
+++ upp-pub-provisioner/sh/substitute-ft-env-variables.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-while read variable ; do
+for variable in `cat /tmp/ft-env-variables` ; do
     # this splits the key value pairs into two separate variables
     # for more info on how it works:
-    # https://stackoverflow.com/a/1521498
-    # https://unix.stackexchange.com/a/53315
-    key=$(cut -d '=' -f 1 <<< "$variable")
-    value=$(cut -d '=' -f 2- <<< "$variable")
+    # http://stackoverflow.com/a/19482947
+    key=${variable%=*}
+    value=${variable##*=}
 
-    sed -i "s|'\$${key}'|'${value}'|g" /tmp/instance_user_data.yaml
-done < /tmp/ft-env-variables
+    sed -i "s|\$${key}|${value}|g" /tmp/persistent_instance_user_data.yaml
+done
