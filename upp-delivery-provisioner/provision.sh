95d9d14a1274eb173677b55ffeb569f2cd1b7613
Dhas <jermila.dhas@ft-mw4834.ad.ft.com>
2017-06-15 12:22:31 +0100

    Set missing key in the provisioner

--- upp-delivery-provisioner/provision.sh
+++ upp-delivery-provisioner/provision.sh
@@ -41,6 +41,7 @@ echo $VAULT_PASS > /vault.pass && ansible-playbook -i ~/.ansible_hosts /ansible/
   aws_es_content_endpoint=${AWS_ES_CONTENT_ENDPOINT} \
   methode_api=${METHODE_API} \
   aggregate_concept_bucket=${AGGREGATE_CONCEPT_BUCKET} \
+  aggregate_concept_queue=${AGGREGATE_CONCEPT_QUEUE} \
   neo4j_fleet_endpoint=${NEO4J_FLEET_ENDPOINT} \
   branch_name=${BRANCH_NAME:=master}" \
   --vault-password-file=/vault.pass
