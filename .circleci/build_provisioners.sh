e868ba37b65759dcb3ea73895a3c2adc41153585
Euan Finlay <euan.finlay@ft.com>
2017-07-31 17:41:35 +0100

    move no builds message outside of for loop

--- .circleci/build_provisioners.sh
+++ .circleci/build_provisioners.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+build_count=0
+
 for provisioner in *-provisioner ; do
     LATEST_COMMIT=$(git rev-parse HEAD)
-    PROVISIONER_COMMIT=$(git log -1 --format=format:%H --full-diff ${provisioner})
-    
-    build_count=0
+    PROVISIONER_COMMIT=$(git log -1 --format=format:%H --full-diff ${provisioner}) 
 
     # trigger a build if the latest commit matches the provisioner
     # and CircleCI configuration exists for the provisioner
@@ -20,9 +20,8 @@ for provisioner in *-provisioner ; do
         https://circleci.com/api/v1.1/project/github/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/tree/${CIRCLE_BRANCH}
 
     fi
-
-    if [[ "${build_count}" == 0 ]] ; then
-        echo No builds triggered.
-    fi
-
 done
+
+if [[ "${build_count}" == 0 ]] ; then
+    echo No builds triggered.
+fi
