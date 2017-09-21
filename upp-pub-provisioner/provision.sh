5f18962c065452249025f2f9af1d6bd61656f059
Euan Finlay <euan.finlay@ft.com>
2017-09-21 16:50:00 +0100

    sort env vars alphabetically

--- upp-pub-provisioner/provision.sh
+++ upp-pub-provisioner/provision.sh
@@ -14,48 +14,48 @@ AMI=`curl -s https://coreos.com/dist/aws/aws-stable.json | jq --arg region $AWS_
 ZONES=(`aws ec2 describe-availability-zones --region $AWS_DEFAULT_REGION | jq -r '.AvailabilityZones[].ZoneName'`)
 
 echo $VAULT_PASS > /vault.pass && ansible-playbook -i ~/.ansible_hosts /ansible/aws_coreos_site.yml --extra-vars " \
-  clusterid=$CLUSTERID \
   ami=$AMI \
-  zones=$ZONES \
-  region=$AWS_DEFAULT_REGION \
-  token=$TOKEN_URL \
-  services_definition_root_uri=${SERVICES_DEFINITION_ROOT_URI:=https://raw.githubusercontent.com/Financial-Times/pub-service-files/master/} \
+  authors_bertha_url=${AUTHORS_BERTHA_URL} \
   aws_access_key_id=$AWS_ACCESS_KEY_ID \
+  aws_image_monitor_test_uuid=$AWS_MONITOR_TEST_UUID \
   aws_secret_access_key=$AWS_SECRET_ACCESS_KEY \
+  binary_s3_bucket=${BINARY_S3_BUCKET} \
   binary_writer_bucket=$BINARY_WRITER_BUCKET \
+  branch_name=${BRANCH_NAME:=master} \
+  carousel_bucket=${CAROUSEL_BUCKET} \
+  carousel_enabled=${CAROUSEL_ENABLED} \
+  clusterid=$CLUSTERID \
+  coco_image_monitor_test_uuid=$COCO_MONITOR_TEST_UUID \
   concepts_rw_s3_bucket=$CONCEPTS_RW_S3_BUCKET \
   concepts_rw_s3_bucket_region=$CONCEPTS_RW_S3_BUCKET_REGION \
-  aws_image_monitor_test_uuid=$AWS_MONITOR_TEST_UUID \
-  coco_image_monitor_test_uuid=$COCO_MONITOR_TEST_UUID \
-  varnish_access_credentials=${CLUSTER_BASIC_HTTP_CREDENTIALS} \
-  authors_bertha_url=${AUTHORS_BERTHA_URL} \
-  roles_bertha_url=${ROLES_BERTHA_URL} \
+  concordances_dynamodb_table=${CONCORDANCES_DYNAMODB_TABLE} \
+  concordances_topic_arn=${CONCORDANCES_TOPIC_ARN} \
+  delivery_clusters_http_credentials=${DELIVERY_CLUSTERS_HTTP_CREDENTIALS}\
+  delivery_clusters_urls=${DELIVERY_CLUSTERS_URLS} \
   environment_tag=${ENVIRONMENT_TAG:=default} \
   environment_type=${ENVIRONMENT_TYPE:=p} \
-  tme_host=${TME_HOST:=tme.ft.com} \
-  delivery_clusters_urls=${DELIVERY_CLUSTERS_URLS} \
-  s3_image_bucket_urls=${S3_IMAGE_BUCKET_URLS} \
-  delivery_clusters_http_credentials=${DELIVERY_CLUSTERS_HTTP_CREDENTIALS}\
-  binary_s3_bucket=${BINARY_S3_BUCKET} \
+  pam_api_key=${PAM_API_KEY}\
+  pam_credential_validation_uuid=${PAM_CREDENTIAL_VALIDATION_UUID} \
+  pam_maicm_validation_url=${PAM_MAICM_VALIDATION_URL} \
   pam_mam_validation_url=${PAM_MAM_VALIDATION_URL} \
   pam_mcpm_validation_url=${PAM_MCPM_VALIDATION_URL} \
   pam_mimm_validation_url=${PAM_MIMM_VALIDATION_URL} \
   pam_mlm_validation_url=${PAM_MLM_VALIDATION_URL} \
-  pam_maicm_validation_url=${PAM_MAICM_VALIDATION_URL} \
+  pam_validator_credentials=${PAM_VALIDATOR_CREDENTIALS} \
   pam_video_validation_url=${PAM_VIDEO_VALIDATION_URL} \
   pam_wam_validation_url=${PAM_WAM_VALIDATION_URL} \
-  pam_validator_credentials=${PAM_VALIDATOR_CREDENTIALS} \
-  pam_api_key=${PAM_API_KEY}\
-  pam_credential_validation_uuid=${PAM_CREDENTIAL_VALIDATION_UUID} \
-  synthetic_article_uuid=${SYNTHETIC_ARTICLE_UUID} \
-  synthetic_article_payload=${SYNTHETIC_ARTICLE_PAYLOAD:=/com/ft/syntheticpublicationmonitor/templates/article-payload.json} \
-  synthetic_list_uuid=${SYNTHETIC_LIST_UUID} \
-  carousel_bucket=${CAROUSEL_BUCKET} \
-  carousel_enabled=${CAROUSEL_ENABLED} \
-  concordances_dynamodb_table=${CONCORDANCES_DYNAMODB_TABLE} \
-  concordances_topic_arn=${CONCORDANCES_TOPIC_ARN} \
+  region=$AWS_DEFAULT_REGION \
+  roles_bertha_url=${ROLES_BERTHA_URL} \
+  s3_image_bucket_urls=${S3_IMAGE_BUCKET_URLS} \
+  services_definition_root_uri=${SERVICES_DEFINITION_ROOT_URI:=https://raw.githubusercontent.com/Financial-Times/pub-service-files/master/} \
+  smartlogic_api_key=${SMARTLOGIC_API_KEY} \
   smartlogic_base_url=${SMARTLOGIC_BASE_URL} \
   smartlogic_model=${SMARTLOGIC_MODEL} \
-  smartlogic_api_key=${SMARTLOGIC_API_KEY} \
-  branch_name=${BRANCH_NAME:=master}" \
+  synthetic_article_payload=${SYNTHETIC_ARTICLE_PAYLOAD:=/com/ft/syntheticpublicationmonitor/templates/article-payload.json} \
+  synthetic_article_uuid=${SYNTHETIC_ARTICLE_UUID} \
+  synthetic_list_uuid=${SYNTHETIC_LIST_UUID} \
+  tme_host=${TME_HOST:=tme.ft.com} \
+  token=$TOKEN_URL \
+  varnish_access_credentials=${CLUSTER_BASIC_HTTP_CREDENTIALS} \
+  zones=$ZONES" \
   --vault-password-file=/vault.pass
