8b5a1bb7a3f0f89b8ea5e7658f5e357c97fb5181
Sorin Buliarca <sorin.buliarca@ft.com>
2017-09-26 15:00:01 +0300

    Added configs for content-prod

---
+++ upp-neo4j-provisioner/aws_configs/content-prod_eu-west-1.sh
@@ -0,0 +1,4 @@
+export VPC_ID="vpc-ee57bf89"
+export SUBNET1="subnet-901020c8"
+export SUBNET2="subnet-5f00f438"
+export SUBNET3="subnet-8cbe4fc5"
---
+++ upp-neo4j-provisioner/aws_configs/content-prod_us-east-1.sh
@@ -0,0 +1,4 @@
+export VPC_ID="vpc-4bc03f32"
+export SUBNET1="subnet-f9fcc99c"
+export SUBNET2="subnet-a8dc1384"
+export SUBNET3="subnet-d63d859e"
