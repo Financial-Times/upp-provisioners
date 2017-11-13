675e109104bff0ee701e5147a70ea5eff79fb58c
Euan Finlay <euan.finlay@ft.com>
2017-11-13 14:27:13 +0000

    base64 encode user data

--- upp-jenkins-provisioner/sh/provision.sh
+++ upp-jenkins-provisioner/sh/provision.sh
@@ -3,6 +3,9 @@
 # Create Ansible vault credentials
 echo ${VAULT_PASS} > /ansible/vault.pass
 
+# base64 encode EC2 user data
+/bin/base64 /cloudformation/userdata.sh | tr -d '\n' > /cloudformation/userdata.base64
+
 cd /ansible
 
 ansible-playbook --vault-password-file=vault.pass provision.yml --extra-vars "\
