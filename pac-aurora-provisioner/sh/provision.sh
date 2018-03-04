f328a6b3539df6c1438e9cc5ff404ca0db1067e1
Jermila Paul Dhas <jpd@JermilasMacBook.beta.zed>
2018-03-04 17:37:54 +0000

    Provision credentials as env variables for pac aurora

--- pac-aurora-provisioner/sh/provision.sh
+++ pac-aurora-provisioner/sh/provision.sh
@@ -6,6 +6,8 @@ echo ${VAULT_PASS} > /ansible/vault.pass
 cd /ansible
 
 ansible-playbook --vault-password-file=vault.pass provision.yml --extra-vars "\
+aws_access_key=${AWS_ACCESS_KEY} \
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY} \
 cluster=${CLUSTER} \
 pac_db_user_password=${PAC_DB_USER_PASSWORD} \
 environment_type=${ENVIRONMENT_TYPE} \
