94adf4fb756a46f774104b58a109e07e22210a66
JPD <JermilaPaulDhas@users.noreply.github.com>
2018-03-06 11:17:11 +0000

    Pass provisioner credentials as env variables (#172)

--- upp-factset-provisioner/sh/provision.sh
+++ upp-factset-provisioner/sh/provision.sh
@@ -8,4 +8,6 @@ ansible-playbook --vault-password-file=vault.pass provision.yml --extra-vars "\
 environment_name=${ENVIRONMENT_NAME} \
 environment_tag=${ENVIRONMENT_TAG} \
 db_master_password=${MASTER_PASSWORD} \
-aws_account=${AWS_ACCOUNT}"
+aws_account=${AWS_ACCOUNT} \
+aws_acces_key=${AWS_ACCESS_KEY} \
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY} "
