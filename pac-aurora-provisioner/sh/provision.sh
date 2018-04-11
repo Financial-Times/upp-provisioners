520512b36d92b96b8b09553bc15851657a528e61
Galia.Rimon <galia.rimon@ft.com>
2018-04-11 11:31:05 +0100

    initial changes for aurora DR

--- pac-aurora-provisioner/sh/provision.sh
+++ pac-aurora-provisioner/sh/provision.sh
@@ -9,6 +9,7 @@ ansible-playbook --vault-password-file=vault.pass provision.yml --extra-vars "\
 aws_access_key=${AWS_ACCESS_KEY} \
 aws_secret_access_key=${AWS_SECRET_ACCESS_KEY} \
 cluster=${CLUSTER} \
+cluster_suffix=${CLUSTER_SUFFIX} \
 pac_db_user_password=${PAC_DB_USER_PASSWORD} \
 environment_type=${ENVIRONMENT_TYPE} \
 source_snapshot=${SOURCE_SNAPSHOT} "
