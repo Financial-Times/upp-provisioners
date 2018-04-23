2a51605ede6a511984f022d68231f5180b31302f
Gallal Sharaf Fareh <email@gallal.com>
2018-04-23 16:12:31 +0100

    Deploy a CloudWatch alarm to monitor concepts publishing queues overflow

--- upp-concept-publishing-provisioner/sh/provision.sh
+++ upp-concept-publishing-provisioner/sh/provision.sh
@@ -9,4 +9,5 @@ environment_type=${ENVIRONMENT_TYPE} \
 is_multi_region=${IS_MULTI_REGION} \
 aws_secondary_region=${AWS_SECONDARY_REGION} \
 aws_access_key=${AWS_ACCESS_KEY} \
-aws_secret_access_key=${AWS_SECRET_ACCESS_KEY} "
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY} \
+sqs_concept_max_depth=${SQS_CONCEPT_MAX_DEPTH}"
