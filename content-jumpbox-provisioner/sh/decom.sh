cbf6fcfe6d7b9946b011ce0b200d77f1d42ebeb7
Jermila Dhas <dhas.jermila@gmail.com>
2018-07-10 10:19:14 +0100

    finaly tweaks to the provisioner  Please enter the commit message for your changes. Lines starting

--- content-jumpbox-provisioner/sh/decom.sh
+++ content-jumpbox-provisioner/sh/decom.sh
@@ -2,7 +2,7 @@
 
 cd /ansible
 
-ansible-playbook provision.yml --extra-vars "\
+ansible-playbook decom.yml --extra-vars "\
 environment_type=${ENVIRONMENT_TYPE} \
 aws_region=${AWS_REGION} \
 aws_access_key=${AWS_ACCESS_KEY} \
