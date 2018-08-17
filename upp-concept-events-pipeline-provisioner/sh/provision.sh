b1ed287d9448ad09834fffd5c16f4650715e8a15
George Antoniadis <george@noodles.gr>
2018-08-17 10:56:51 +0100

    Add concept events pipeline provisioner (#192)
    * Add concept events pipeline provisioner
    
    * Fix code review comments

---
+++ upp-concept-events-pipeline-provisioner/sh/provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo $VAULT_PASS > /ansible/vault.pass
+
+cd /ansible
+
+ansible-playbook --vault-password-file=vault.pass provision.yml --extra-vars "\
+environment_name=${ENVIRONMENT_NAME} \
+environment_tag=${ENVIRONMENT_TAG} \
+aws_default_region=${AWS_DEFAULT_REGION} \
+db_master_password=${MASTER_PASSWORD} \
+aws_account=${AWS_ACCOUNT} \
+aws_access_key=${AWS_ACCESS_KEY} \
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}"
