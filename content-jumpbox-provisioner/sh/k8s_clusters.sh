32cd1e52d066902b5c3e45003402ed4f5c9c969a
Jermila Dhas <dhas.jermila@gmail.com>
2018-09-24 17:03:47 +0100

    jumpbox changes for ops

---
+++ content-jumpbox-provisioner/sh/k8s_clusters.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+__kube_ps1()
+{
+    # Get current context if KUBECONFIG is set
+    if [ -n "$KUBECONFIG" ] ; then
+        CONTEXT=$(kubectl config current-context)
+        
+        if [ -n "$CONTEXT" ] ; then
+            echo "${CONTEXT}"
+        fi
+    fi
+}
+
+echo -e "
+~~~
+
+\e[31mKubernetes Instructions\e[39m
+
+To show available clusters:
+list-clusters
+
+To connect to a cluster:
+source switch-cluster upp-delivery-prod-eu
+"
+
+export PS1='[\u (k8s: $(__kube_ps1)) \W]\$ '
