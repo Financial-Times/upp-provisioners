2e35c28060d451396f9379e34a2c5983d6757f2d
Jermila Dhas <dhas.jermila@gmail.com>
2018-10-03 22:15:34 +0100

    Remove files not used Update instructions for Ops

--- content-jumpbox-provisioner/k8s_ssh/k8s_clusters.sh
+++ content-jumpbox-provisioner/k8s_ssh/k8s_clusters.sh
@@ -10,4 +10,7 @@ list-clusters
 
 To connect to a cluster:
 source cluster-login.sh upp-delivery-prod-eu
+
+\e[31m * Copy the link to a browser and enter your AD credentials \e[39m
+\e[31m * Get the token and enter it in the terminal\e[39m
 \"
