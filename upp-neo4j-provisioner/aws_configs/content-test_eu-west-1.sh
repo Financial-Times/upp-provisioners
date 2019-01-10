1c664d5f2e6e8fa5087553990f42ee00025233c3
Euan Finlay <euan.finlay@ft.com>
2019-01-10 18:12:35 +0200

    :fire: removing neo4j provisioner

--- upp-neo4j-provisioner/aws_configs/content-test_eu-west-1.sh
+++
@@ -1,5 +0,0 @@
-export VPC_ID="vpc-f75fb790"
-export VPC_CIDR="10.172.32.0/21"
-export SUBNET1="subnet-2a1de94d"
-export SUBNET2="subnet-c1b94888"
-export SUBNET3="subnet-932717cb"
--- upp-neo4j-provisioner/puppet/neo4jha/manifests/jumpbox/install.pp
+++
@@ -1,7 +0,0 @@
-class neo4jha::jumpbox::install {
-  package { 'requests':
-    ensure    => '2.13.0',
-    provider  => 'pip'
-  }
-
-}
