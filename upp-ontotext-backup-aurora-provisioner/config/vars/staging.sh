86f03ad8e358462038312717f6ed627b0fc0d4c2
Kalin Arsov <kalin.arsov@ft.com>
2020-10-21 12:59:14 +0300

    Create Ontotext backup RDS provisioner

---
+++ upp-ontotext-backup-aurora-provisioner/config/vars/staging.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+set -euo pipefail
+IFS=$'\n\t'
+
+ENVIRONMENT_DESC="Staging"
+RESOURCES_PREFIX="upp"
+
+AWS_REGION="eu-west-1"
+VPC_ID="vpc-ee57bf89"
+RESOURCES_SG_ID="sg-39ef7b40"
+RDS_SUBNETS="subnet-024e2e32aefaa01c5, subnet-02526d7213a359f48, subnet-0f2c8a8f1e3db176a"
+
+ENVIRONMENT_TAG="t"
+SYSTEM_CODE_TAG="upp"
+TEAM_DL_TAG="universal.publishing.platform@ft.com"
+
+CF_STACK_NAME="${RESOURCES_PREFIX}-ontotext-backup-rds-${ENVIRONMENT_NAME}"
