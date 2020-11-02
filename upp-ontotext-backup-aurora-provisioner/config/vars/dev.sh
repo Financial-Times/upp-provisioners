9f4bf3722614b3e2dc4283bdf2afd143ea53b54a
Kalin Arsov <kalin.arsov@ft.com>
2020-11-02 13:54:46 +0200

    Add incoming CIDR range conf for Ontotext Aurora provisioner

--- upp-ontotext-backup-aurora-provisioner/config/vars/dev.sh
+++ upp-ontotext-backup-aurora-provisioner/config/vars/dev.sh
@@ -9,6 +9,8 @@ RESOURCES_PREFIX="upp"
 
 AWS_REGION="eu-west-1"
 VPC_ID="vpc-f75fb790"
+CLUSTER_CIDR="10.169.0.0/18"
+CLUSTER_CIDR_DESC="FT-Content-Platform-Test vpc-f75fb790 CIDR"
 RESOURCES_SG_ID="sg-8bcb5ff2"
 RDS_SUBNETS="subnet-020bff57b77ad7438, subnet-0d1ca6b04133a3ad2, subnet-048030b5219e45d9b"
 
--- upp-ontotext-backup-aurora-provisioner/config/vars/staging.sh
+++ upp-ontotext-backup-aurora-provisioner/config/vars/staging.sh
@@ -9,6 +9,8 @@ RESOURCES_PREFIX="upp"
 
 AWS_REGION="eu-west-1"
 VPC_ID="vpc-ee57bf89"
+CLUSTER_CIDR="10.169.64.0/18"
+CLUSTER_CIDR_DESC="FT-Content-Platform-Prod vpc-ee57bf89 CIDR"
 RESOURCES_SG_ID="sg-39ef7b40"
 RDS_SUBNETS="subnet-024e2e32aefaa01c5, subnet-02526d7213a359f48, subnet-0f2c8a8f1e3db176a"
 
