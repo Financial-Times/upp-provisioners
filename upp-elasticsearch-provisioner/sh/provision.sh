7cb44bf7a825a883d493b65dde6e8a78f4f3fc05
Ivan Nikolov <ivan.nikolov@ft.com>
2023-03-13 14:13:44 +0200

    Add  OpenSearch cloud formation for CCF content

--- upp-elasticsearch-provisioner/sh/provision.sh
+++ upp-elasticsearch-provisioner/sh/provision.sh
@@ -9,6 +9,16 @@ if [ ! -f /cloudformation/${CF_TEMPLATE}.yml ] ; then
     exit 1
 fi
 
+env_vars="/vars/${CF_TEMPLATE}-${DELIVERY_CLUSTER}.yml"
+# Validate that ${CF_TEMPLATE}.yml exists
+if [ ! -f ${env_vars} ] ; then
+    echo -e "Error - ${env_vars}.yml doesn't exist.\n"
+    echo -e "Valid \$CF_TEMPLATE values are:"
+    ls /vars/ | sed 's/.yml//g'
+    echo
+    env_vars=""
+fi
+
 # Create Ansible vault credentials
 echo ${VAULT_PASS} > /ansible/vault.pass
 
@@ -22,4 +32,7 @@ environment_type=${ENVIRONMENT_TYPE} \
 restore_es_snapshot=${RESTORE_ES_SNAPSHOT} \
 aws_access_key=${AWS_ACCESS_KEY} \
 aws_secret_access_key=${AWS_SECRET_ACCESS_KEY} \
-aws_account=${AWS_ACCOUNT}"
+aws_account=${AWS_ACCOUNT} \
+env_vars=${env_vars}
+"
+
